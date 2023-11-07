29e793926e7bb5e62c8dece4d057bea36ff6965d
Dreadnout <rfiflaa@gmail.com>
2023-11-07 23:40:17 +0300

    Example 11

--- Exaple008_introMethod/Program.cs
+++ Exaple008_introMethod/Program.cs
@@ -8,18 +8,28 @@
 
 
 
-int a1 = 155555;
+int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
 int b2 = 23;
 int c2 = 33;
-int a3 = 122;
+int a3 = 1;
 int b3 = 23;
-int c3 = 33;
+int c3 = 345;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+
+
+
+
 System.Console.WriteLine(max);
